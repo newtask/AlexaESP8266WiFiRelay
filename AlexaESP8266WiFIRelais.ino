ba3a98334c5a99edc2aa6b312083b48974f726b7
new_task <a.taske@gmx.de>
2018-02-09 22:26:30 +0100

    added ino file with todos

---
+++ AlexaESP8266WiFIRelais.ino
@@ -0,0 +1,20 @@
+/***
+ * by Andreas Taske (andreas@taske.de) 2018
+ * **/
+
+// TODO:
+// - add relais control
+// - add control led
+// - add button that controls relais
+// - add wifimanager
+// - add spdiff settings store
+// - add mqtt client
+// - add webserver that controls relais
+// - add alexa belkin simulator
+
+void setup() {
+ 
+}
+
+void loop() {
+}
